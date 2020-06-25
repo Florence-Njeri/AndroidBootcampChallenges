616ed974bf12def087ba71e43420b9ddb37d75bb
Florence-Njeri <flonsie17@gmail.com>
2020-06-25 19:50:14 +0300

    save drops to the database using ContentValues

--- Week5/start/app/src/main/java/com/raywenderlich/android/datadrop/model/SQLiteRepository.kt
+++ Week5/start/app/src/main/java/com/raywenderlich/android/datadrop/model/SQLiteRepository.kt
@@ -1,10 +1,14 @@
 package com.raywenderlich.android.datadrop.model
 
+import android.content.ContentValues
 import com.raywenderlich.android.datadrop.app.DataDropApplication
+import com.raywenderlich.android.datadrop.model.DropDbSchema.DropTable
 
 class SQLiteRepository : DropRepository {
     val database = DropDbHelper(DataDropApplication.getAppContext()).writableDatabase
     override fun addDrop(drop: Drop) {
+        val contentValues = getDropContentValues(drop)
+        database.insert(DropTable.NAME, null, contentValues)
 
     }
 
@@ -18,5 +22,16 @@ class SQLiteRepository : DropRepository {
 
     override fun clearAllDrops() {
 
+    }
+
+    //Convert drop to a contentValue object
+
+    private fun getDropContentValues(drop: Drop): ContentValues {
+        val contentValues = ContentValues()
+        contentValues.put(DropTable.Columns.ID, drop.id)
+        contentValues.put(DropTable.Columns.LATITUDE, drop.latLng.latitude)
+        contentValues.put(DropTable.Columns.LONGITUDE, drop.latLng.longitude)
+        contentValues.put(DropTable.Columns.DROP_MESSAGE, drop.dropMessage)
+        return contentValues
     }
 }
