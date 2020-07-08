c8df1b80bb12a5e391def6399c123f158fe91ecf
Florence-Njeri <flonsie17@gmail.com>
2020-07-08 17:10:48 +0300

    add internet and connectivity permissions as well as add a NetworkChecker class
    This class is responsible for checking the type of network the use is connected to either mobile,VPN or WIFI.
    
    If any of the above are true then it is determined that they can indeed send and receive data from the server

---
+++ week 7/ANF-P01E02 - Set Up The Project/P01E02 - Set Up The Project/starter/app/src/main/java/com/raywenderlich/android/taskie/networking/NetworkStatusChecker.kt
@@ -0,0 +1,25 @@
+package com.raywenderlich.android.taskie.networking
+
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+
+class NetworkStatusChecker(private val connectivityManager: ConnectivityManager) {
+//    ConnectivityManager is used the check if there s good internet connection
+
+    inline fun performIfConnectedToTheInternet(action: ()->Unit){
+        if(hasInternetConnection()){
+            action()
+        }
+    }
+
+    fun hasInternetConnection():Boolean{
+        val network = connectivityManager.activeNetwork ?: return false
+        //If there is a mobile network check if connected to Mobile,WIFI or a VPN
+        val capabilities = connectivityManager.getNetworkCapabilities(network) ?: return false
+
+        //If any is true then it has the capability to fetch data from the server
+        return capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)
+                || capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)
+                || capabilities.hasTransport(NetworkCapabilities.TRANSPORT_VPN)
+    }
+}
