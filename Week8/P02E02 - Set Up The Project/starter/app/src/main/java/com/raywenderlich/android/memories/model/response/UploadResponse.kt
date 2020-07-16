aec6862ec2c352cc24acf277a6c71ca6a2964a60
Florence-Njeri <flonsie17@gmail.com>
2020-07-16 15:50:19 +0300

    add dependencies

--- Week8/P02E02 - Set Up The Project/starter/app/src/main/java/com/raywenderlich/android/memories/model/response/UploadResponse.kt
+++
@@ -1,6 +0,0 @@
-package com.raywenderlich.android.memories.model.response
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class UploadResponse(val message:String ="", val url:String = "")
