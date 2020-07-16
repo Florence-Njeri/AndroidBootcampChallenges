a2bc87c20db247fded3d4ccec921685424437537
Florence-Njeri <flonsie17@gmail.com>
2020-07-16 16:10:23 +0300

    add the base url and the httpLogging inteceptor to the networkModule

--- Week8/Koin/app/src/main/java/com/raywenderlich/spacedaily/di/NetworkModule.kt
+++ Week8/Koin/app/src/main/java/com/raywenderlich/spacedaily/di/NetworkModule.kt
@@ -1,8 +1,14 @@
 package com.raywenderlich.spacedaily.di
 
+import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 val networkModue = module {
-    single(named("BASE_URL")) { }
+    single(named("BASE_URL")) { "https://api.nasa.gov/planetary/" }
+    single {
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.level = HttpLoggingInterceptor.Level.BODY
+        interceptor
+    }
 }
