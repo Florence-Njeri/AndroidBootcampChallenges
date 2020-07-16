e89864263cb3fe90da5647fa6747953bcb5337bb
Florence-Njeri <flonsie17@gmail.com>
2020-07-16 16:14:40 +0300

    add an okhttp client and add out logging interceptor

--- Week8/Koin/app/src/main/java/com/raywenderlich/spacedaily/di/NetworkModule.kt
+++ Week8/Koin/app/src/main/java/com/raywenderlich/spacedaily/di/NetworkModule.kt
@@ -1,14 +1,30 @@
 package com.raywenderlich.spacedaily.di
 
+import com.raywenderlich.spacedaily.BuildConfig
+import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
+import java.util.concurrent.TimeUnit
 
 val networkModue = module {
     single(named("BASE_URL")) { "https://api.nasa.gov/planetary/" }
+    //Create the logging interceptor
     single {
         val interceptor = HttpLoggingInterceptor()
         interceptor.level = HttpLoggingInterceptor.Level.BODY
         interceptor
+    }
+
+    //Add the Okhttp client
+    single {
+        val client = OkHttpClient.Builder()
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .writeTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+        if (BuildConfig.DEBUG) {
+            client.addInterceptor(get<HttpLoggingInterceptor>())
+        }
+        client.build()
     }
 }
