98a40cfdd2ac4b74cd02099f51a0ccbe01b85644
Florence-Njeri <flonsie17@gmail.com>
2020-07-24 11:54:38 +0300

    initial commit

--- Week8/Koin/app/src/main/java/com/raywenderlich/spacedaily/MainActivity.kt
+++ Week8/Koin/app/src/main/java/com/raywenderlich/spacedaily/MainActivity.kt
@@ -24,11 +24,13 @@ package com.raywenderlich.spacedaily
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+    DailyPhotoView(mainLayout)
   }
 }
